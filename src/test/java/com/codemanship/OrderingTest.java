8a90cce7e817ffcedd696de3d7459a70156b166c
davide <telapo@users.noreply.github.com>
2026-04-11 12:14:09 +0200

    refactor code

--- src/test/java/com/codemanship/OrderingTest.java
+++ src/test/java/com/codemanship/OrderingTest.java
@@ -1,5 +1,6 @@
 package com.codemanship;
 
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,8 +11,8 @@ public class OrderingTest {
 
     @Test
     void addHoldOnItem() {
-        Product product = new Product(327, 7, 0);
-        Order order = new Order(List.of());
+        Product product = createProduct();
+        Order order = createEmptyOrder();
 
         order.add(product, 1);
 
@@ -20,13 +21,23 @@ public class OrderingTest {
 
     @Test
     void addingItemWithSufficientStock() {
-        Product product = new Product(327, 7, 0);
-        Order order = new Order(List.of());
+        Product product = createProduct();
+        Order order = createEmptyOrder();
 
         order.add(product, 1);
 
         assertEquals(1, order.items().size());
         assertEquals(product.id(), order.items().get(0).product().id());
         assertEquals(1, order.items().get(0).quantity());
+    }
+
+    private static Product createProduct() {
+        Product product = new Product(327, 7, 0);
+        return product;
+    }
+
+    private static Order createEmptyOrder() {
+        Order order = new Order(List.of());
+        return order;
     }
 }
